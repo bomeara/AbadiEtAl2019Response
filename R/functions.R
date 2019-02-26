9f27c038f6754875f9328cb35790e3cf820b18c4
Brian O'Meara <omeara.brian@gmail.com>
2019-02-25 23:27:20 -0500

    starting batch runs

--- R/functions.R
+++ R/functions.R
@@ -7,6 +7,54 @@
 #' @export
 summarizeData <- function(dir="c3") {
   files <- list.files(paste0("data/",dir), pattern="*phy")
+	result <- data.frame()
+	for (file_index in seq_along(files)) {
+		local_run <- NULL
+		try(local_run <- runTree(paste0("data/",dir, "/", files[file_index])))
+		if(!is.null(local_run)) {
+			result <- plyr::rbind.fill(result, local_run)
+			save(result, file="docs/results.rda")
+		}
+
+	}
+	return(result)
+}
+
+#' Run individual step
+#'
+#' Gets phyml tree, bionj, etc.
+#'
+#' @param infile The file to pull in (include path)
+#' @return data.frame of the trees and other info
+#' @export
+runTree <- function(infile) {
+	dna <- ape::read.dna(file=infile)
+	bionj_tree <- ape::bionj(ape::dist.dna(dna))
+	upgma_tree <- phangorn::upgma(dna)
+	jc_tree <- runPhyml(infile, model=" -m JC69 -f m")
+	gtr_ig_tree <- runPhyml(infile, model=" -m GTR -f m -v e -a e")
+	result <- data.frame(ntax=nrow(dna), nsites=ncol(dna), pars_inf_count=ips::pis(dna, what="absolute"), pars_inf_fraction=ips::pis(dna, what="fraction"), bionj=ape::write.tree(bionj_tree), upgma=ape::write.tree(upgma_tree), jc=ape::write.tree(jc_tree), gtrig=ape::write.tree(gtr_ig_tree))
+	return(result)
+}
+
+#' Run PHYML model
+#'
+#' Run PHYML using a model
+#'
+#' Model:
+#' JC: " -m JC69 -f m"
+#' GTR+I+G: " -m GTR -f m -v e -a e"
+#'
+#' @param infile Path to file and file name
+#' @param model Model settings for phyml
+#' @return Inferred tree
+#' @export
+runPhyml <- function(infile, model=" -m JC69 -f m") {
+	system(paste0("phyml -i ", infile, model))
+	phy <- ape::read.tree(paste0(infile, '_phyml_tree.txt'))
+	system(paste0("rm ", paste0(infile, '_phyml_tree.txt')))
+	system(paste0("rm ", paste0(infile, '_phyml_stats.txt')))
+	return(phy)
 }
 
 #' Make a pdf using pandoc
