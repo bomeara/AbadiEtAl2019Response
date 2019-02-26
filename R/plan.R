26b8435fd44751246f760c87ba0247efec2019ff
Brian OMeara <omeara.brian@gmail.com>
2019-02-26 16:31:26 -0500

    starting up with more trees

--- R/plan.R
+++ R/plan.R
@@ -1,7 +1,7 @@
 report <- drake_plan(
-  c3_summary = summarizeData(maxtree=50),
+  c3_summary = summarizeData(maxtree=250),
   c3_tree_summary = treetree(c3_summary),
-  empirical_summary = summarizeData(dir="empirical", treesearch=FALSE, maxtree=100),
+  empirical_summary = summarizeData(dir="empirical", treesearch=FALSE),
   otol_summary = otol_trees(),
   report = knitr::knit(drake::knitr_in("docs/index.Rmd"), drake::file_out("docs/index.md"), quiet = TRUE),
   pdf_report = render_pdf("index.md", "index.pdf", "docs", report),
